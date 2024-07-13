7b7f0d479cffe4319902f9bf9135a0d7ebafb6c6
annu <annupawadia@gmail.com>
2024-07-13 13:27:10 +0530

    started a new component

--- my-react-app/src/components/Navbar.js
+++ my-react-app/src/components/Navbar.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import {Button,Menu,Avatar,Typography} from 'antd'
+import {Link} from 'react-router-dom'
+import {HomeOutlined,MoneyCollectOutlined,Bulboutlined,FundOutlned,} from 'antd'
+import icon from '../images/cryptocurrency.png';
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='nav-container'>
+      <div className='logo-container'>
+<Avatar src={icon} size="large"/>
+<Typography.Title leel={2} className='logo'>
+  <Link to='/'>Crytoverse</Link>
+</Typography.Title>
+      </div>
+    </div>
   )
 }
 
